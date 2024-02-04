579d2def757aeb5850e7cb57669aa29759dc3a34
migueacastro <miguelcastropadron@gmail.com>
2024-02-03 20:09:18 -0400

    Setting up database

--- app/dimpro/admin.py
+++ app/dimpro/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Image, Product, ItemQuantity
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(Product)
+admin.site.register(ItemQuantity)
